8791de8adaffec802277a9cf4cf2ef2a7df6a1d6
mamatadash1 <dsh.mamata@gmail.com>
2022-02-15 18:14:15 +0530

    Initial commit eureka

---
+++ src/test/java/com/mamata/fsd/PixogramProjectEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamata.fsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PixogramProjectEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
